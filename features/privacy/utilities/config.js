a83e13d2fc842a7d50b04a839e5b76c95d7cb16b
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2025-06-17 13:51:42 +0530

    Updated for config.js

--- features/privacy/utilities/config.js
+++ features/privacy/utilities/config.js
@@ -1,36 +1,105 @@
+/* config */
 const categories = ['C0001', 'C0002', 'C0003', 'C0004'];
 const categoriesDefault = categories[0];
+const configEndpoint = 'https://cdn.cookielaw.org/consent/';
+const id = 'onetrust';
+const location = 'feds_location';
+const locationURL = 'https://geo2.adobe.com/json/?callback=';
+const url = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
 
-// Consent cookies
-const CONSENT_COOKIE = 'OptanonConsent';
-const INTERACTION_COOKIE = 'OptanonAlertBoxClosed';
-
-// Category groupings
-const CATEGORIES_ALL = [...categories];
-
-// Optional: OneTrust domainId config
-const OT_DOMAIN_ID = window?.fedsConfig?.privacy?.otDomainId || '';
-
-// Fallback geo API
-const GEO_LOCATION_URL = 'https://geo2.adobe.com/json/?callback=';
-
-const EVENTS = {
-  privacyConsent: 'adobePrivacy:PrivacyConsent',
-  privacyConsentCustom: 'adobePrivacy:PrivacyCustom',
-  privacyReject: 'adobePrivacy:PrivacyReject',
+/* restrictions */
+const imsTimeout = 3000;
+const userTags = {
+    age: ['agegroup_under13', 'agegroup_13_15', 'agegroup_16_17'],
+    edu: ['edu', 'edu_k12', 'edu_hed', 'edu_student'],
+};
+const modes = {
+    gpc: 'GPC',
+    group: 'User Group',
+    user: 'User Initiated',
 };
 
-const IMS_TIMEOUT = 3000;
+/* cookies */
+const interaction = 'OptanonAlertBoxClosed';
+const consent = 'OptanonConsent';
 
-// Export what you use!
+/* selectors */
+const banner = '#onetrust-banner-sdk';
+const bannerAccept = `${banner} #onetrust-accept-btn-handler`;
+const bannerClose = '#ot-banner-close';
+const bannerCustomize = `${banner} #onetrust-pc-btn-handler`;
+const bannerIcon = '#ot-cookie-button';
+const bannerReject = `${banner} #onetrust-reject-all-handler`;
+const buttonGroup = `${banner} #onetrust-button-group`;
+const preferenceCenter = '#onetrust-pc-sdk';
+const preferenceCenterTitle = `${preferenceCenter} #pc-title`;
+const preferenceCenterAccept = `${preferenceCenter} .enable-all-btn`;
+const preferenceCenterAlwaysActive = `${preferenceCenter} #first-party-cookies-domain`;
+const preferenceCenterCheckboxes = `${preferenceCenter} .switch-checkbox`;
+const preferenceCenterClose = `${preferenceCenter} .pc-close-button`;
+const preferenceCenterConfirm = `${preferenceCenter} .pc-save-and-close`;
+const preferenceCenterFaqs = `${preferenceCenter} .ot-general-question`;
+const preferenceCenterReject = `${preferenceCenter} .disable-all-btn`;
+const preferenceCenterViewCookies = `${preferenceCenter} .host-item > input`;
+const toastNotification = '#ot-cookie-settings';
+const toastNotificationClose = `${toastNotification} .cs-close`;
+const toastNotificationManage = `${toastNotification} .ot-sdk-show-settings`;
+const wrapper = '#onetrust-consent-sdk';
+const backdrop = `${wrapper} .onetrust-pc-dark-filter`;
+const holdBanner = 'hold-banner';
+
+/* events */
+const privacyConsent = 'adobePrivacy:PrivacyConsent';
+const privacyConsentCustom = 'adobePrivacy:PrivacyCustom';
+const privacyReject = 'adobePrivacy:PrivacyReject';
+const oneTrustReady = 'feds.events.oneTrustReady';
+
 export default {
-  categories,
-  categoriesDefault,
-  CATEGORIES_ALL,
-  CONSENT_COOKIE,
-  INTERACTION_COOKIE,
-  OT_DOMAIN_ID,
-  GEO_LOCATION_URL,
-  EVENTS,
-  IMS_TIMEOUT,
+    categories,
+    categoriesDefault,
+    configEndpoint,
+    id,
+    location,
+    locationURL,
+    url,
+    events: {
+        privacyConsent,
+        privacyConsentCustom,
+        privacyReject,
+        oneTrustReady,
+    },
+    cookies: {
+        consent,
+        interaction,
+    },
+    selectors: {
+        banner,
+        bannerAccept,
+        bannerClose,
+        bannerCustomize,
+        bannerIcon,
+        bannerReject,
+        buttonGroup,
+        preferenceCenter,
+        preferenceCenterTitle,
+        preferenceCenterAccept,
+        preferenceCenterAlwaysActive,
+        preferenceCenterCheckboxes,
+        preferenceCenterClose,
+        preferenceCenterConfirm,
+        preferenceCenterFaqs,
+        preferenceCenterReject,
+        preferenceCenterViewCookies,
+        holdBanner,
+        toastNotification,
+        toastNotificationClose,
+        toastNotificationManage,
+        wrapper,
+        backdrop,
+    },
+    restrictions: {
+        imsTimeout,
+        userTags,
+        modes,
+    },
 };
